6b5fe58543bfade4ab8ddf6a5117227885fc7dd4
bellathebot <bellabot369@gmail.com>
2026-04-06 15:22:45 -0400

    Update trading_system/perps_sync_db.py

---
+++ trading_system/perps_sync_db.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Perps-facing wrapper around the shared SQLite sync entrypoint."""
+
+from trading_system.sync_trading_db import main
+
+
+if __name__ == '__main__':
+    main()
